42d9ffc7c95b5c1c44724173bb620431cf23fc68
TheoV <82026064+TheoV22@users.noreply.github.com>
2021-04-09 19:00:37 +0200

    Add files via upload

--- bmispider.py
+++ bmispider.py
@@ -1,7 +1,8 @@
 import scrapy
-# from bmiscraper.items import BMIscraperItem
-from items import BMIscraperItem
-from scrapy.loader import ItemLoader
+from scrapy import signals
+from pydispatcher import dispatcher
+import json
+import time
 
 
 class BMIMeasurementSpider (scrapy.Spider):
@@ -9,24 +10,70 @@ class BMIMeasurementSpider (scrapy.Spider):
     name = "bmimeasurement"
     start_urls = ["https://s.1688.com/company/company_search.htm?keywords=BMI%B2%E2%C1%BF&spm=a26352%2C13672862"]
 # Careful: the page is encoded with bytes, we can't extract information
+    counter = 0
+    results = {}
+
+    def __init__(self):
+        dispatcher.connect(self.spider_closed, signals.spider_closed)
 
     def parse(self, response):
+    # extract data from the parent page
+
         for products in response.css('div.company-offer-contain'):
-            l = ItemLoader(item = BMIscraperItem(), selector = products)
-
-            l.add_css('name', 'a.company-name')
-            l.add_css('experience', 'span.integrity-year')
-            l.add_css('link', 'a.company-name::attr(href)')
-            l.add_css('price', 'span.price')
-            l.add_css('factory', 'span.factory-tag')
-            # l.add_css('Labels', 'a.company-name::text')
-            # l.add_css('MOQ', 'a.company-name::text')
-            # l.add_css('employees', 'a.company-name::text')
-
-            yield l.load_item()
+            time.sleep(2)
+            yield {
+                'name': products.css('a.company-name::text').get(),
+                'experience': products.css('span.integrity-year::text').get(),
+                'link': products.css('a.company-name').attrib['href'],
+                'price': products.css('span.price').replace('¥','').get(),
+                'factory': products.css('span.factory-tag::text').get(),
+            }
 
         next_page = response.css('a.fui-next').attrib['href']
         # Careful, on 1688, no href is given for the next page
-
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
+
+
+    def parse2(self, response):
+    # extract links of nested webpages from parent page and send them to another parse function
+        time.sleep(2)
+        for products in response.css("div.company-offer-card > div.company-left-card > div.company-info-contain > div.title-container > a.company-name::attr('href')"):
+            yield scrapy.Request(url=products.get(), callback=self.parseNested)
+
+        next_page = response.css('a.fui-next').attrib['href']
+        # Careful, on 1688, no href is given for the next page
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse2)
+
+
+    def parseNested(self, response):
+    # extract data from the nested webpages
+        time.sleep(2)
+        for attr in response.css('div.company-offer-contain'):
+            yield {
+                'Certifications': attr.css('font.data-spm-anchor-id="a262cb.19918180.khuur57j.i5.6f2d3cb72krorG"::text').get(),
+                'MOQ': attr.css('font.data-spm-anchor-id="a262cb.19918180.khuur57j.i0.6f2d3cb79cNLnm"::text').get(),
+                'employees': attr.css('font.data-spm-anchor-id="a262cb.19918180.khuur57j.i8.6f2d3cb72krorG"::text').get(),
+                'PlantArea': attr.css('font.data-spm-anchor-id="a262cb.19918180.khuur57j.i6.6f2d3cb72krorG"::text').get(),
+                'Proofing': attr.css('font.data-spm-anchor-id="a262cb.19918180.khuur57j.i7.6f2d3cb72krorG"::text').get(),
+            }
+
+        self.results[self.counter] = {
+            'name': name,
+            'experience': experience,
+            'link': link,
+            'price': price,
+            'factory': factory,
+            'Certifications': certifications,
+            'MOQ': MOQ,
+            'employees': employees,
+            'PlantArea': PlantArea,
+            'Proofing': Proofing,
+        }
+
+        self.counter += 1
+
+def spider_closed(self, spider):
+    with open('results.json', 'w') as fp:
+        json.dump(self.results, fp)
